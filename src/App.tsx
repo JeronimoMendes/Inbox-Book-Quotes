e7b45f4e87086c22cf8d8cdb57f3671fd244d8a1
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-02-07 00:07:23 +0000

    Add firebase credentials

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import {
 	Route
 } from "react-router-dom";
 import { AddQuotePage } from './pages/AddQuotePage';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+firebase.initializeApp({
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID
+})
 
 function App() {
 	return (
