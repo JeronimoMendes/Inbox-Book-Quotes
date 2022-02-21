ee98ac267266908116bd8c063d77e2d484bec934
Jeronimo Mendes <jeronimo.m.mendes@gmail.com>
2022-02-21 21:54:59 +0000

    Fix email function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,27 +6,32 @@ admin.initializeApp();
 
 const API_KEY = functions.config().sendgrid.key;
 const TEMPLATE_ID = functions.config().sendgrid.template;
+const EMAIL_FROM = functions.config().email.value;
 sgMail.setApiKey(API_KEY);
 
 // Functions
 
 export const sendWeeklyQuote = functions.pubsub.schedule('every saturday 17:00').onRun(async context => {
+	console.log("running function")
 	const userSnapshots = await admin.firestore().collection('users').get();
-
-	const emails = userSnapshots.docs.map(snap => snap.data().email);
-
-	// For each user, send a quote
-	emails.forEach(email => {
-		// fetch random quote from the current user
-		sgMail.send({
-			to: email,
-			from: '',
-			templateId: TEMPLATE_ID,
-			dynamicTemplateData: {
-				text: "", // Quote text here
-				author: "", // Quote author here
-				book: "", // Book title here
-			}
-		});		
+	
+	userSnapshots.forEach(user => {
+		// pick random quote
+		console.log(user.data())
+		console.log("Sending a quote email to user: " + user.data().id);
+		const randomQuoteId = user.data().quotes[Math.floor(Math.random() * user.data().quotes.length)];
+		console.log("Quote: " + randomQuoteId);
+		admin.firestore().doc("quote/" + randomQuoteId).get().then((res) => {
+			sgMail.send({
+				to: user.data().email,
+				from: EMAIL_FROM,
+				templateId: TEMPLATE_ID,
+				dynamicTemplateData: {
+					text: res.get("text"), // Quote text here
+					author: res.get("author"), // Quote author here
+					book: res.get("book"), // Book title here
+				}
+			});
+		});
 	});
 });
